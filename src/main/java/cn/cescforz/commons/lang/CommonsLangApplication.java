dfc31dd6360c2ac7802244fa79f3eb915fe47820
lh <cescforz@foxmail.com>
2019-07-23 17:11:03 +0800

    Initial commit

---
+++ src/main/java/cn/cescforz/commons/lang/CommonsLangApplication.java
@@ -0,0 +1,13 @@
+package cn.cescforz.commons.lang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonsLangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonsLangApplication.class, args);
+    }
+
+}
